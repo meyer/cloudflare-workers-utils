a635d846e54b4673b8c1c9cea854e37ca08debf9
meyer <github.com@meyer.fm>
2023-04-06 23:45:33 -0400

    Make `getUrlForMessage` a bit less needy

--- packages/discord/src/getUrlForMessage.ts
+++ packages/discord/src/getUrlForMessage.ts
@@ -1,5 +1,5 @@
 import type { APIMessage } from 'discord-api-types/v10';
 
-export const getUrlForMessage = (guildId: string, message: APIMessage) => {
+export const getUrlForMessage = (guildId: string, message: Pick<APIMessage, 'channel_id' | 'id'>) => {
   return `https://discord.com/channels/${guildId}/${message.channel_id}/${message.id}`;
 };
